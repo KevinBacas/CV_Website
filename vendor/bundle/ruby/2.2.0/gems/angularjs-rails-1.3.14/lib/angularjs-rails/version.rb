31bcd067d57bdfa96a7a889707a029b05e654e69
kevinbacas <bacas.kevin@hotmail.fr>
2015-03-23 01:39:40 +0100

    Remove caching

--- vendor/bundle/ruby/2.2.0/gems/angularjs-rails-1.3.14/lib/angularjs-rails/version.rb
+++
@@ -1,6 +0,0 @@
-module AngularJS
-  module Rails
-    VERSION = "1.3.14"
-    UNSTABLE_VERSION = "1.3.14"
-  end
-end
--- vendor/bundle/ruby/2.2.0/gems/font-awesome-sass-4.3.2.1/lib/font_awesome/sass/version.rb
+++
@@ -1,5 +0,0 @@
-module FontAwesome
-  module Sass
-    VERSION = '4.3.2.1'
-  end
-end
